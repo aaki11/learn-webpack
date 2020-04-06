e165521d30afc2275ea69e8efab5d3e0499034f7
Aakash Khandelwal <khandelwal.aakash91@gmail.com>
2020-04-06 13:00:38 +0530

    Added support for loading img in webpack

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import _ from 'lodash';
 import './styles.css';
+import Forest from './forestSVG.svg';
 
 function component(){
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello','webpack']);
     element.classList.add('hello');
+    const myIcon = new Image();
+    myIcon.src = Forest;
+    element.appendChild(myIcon);
     return element;
 }
 
